34c563b87a228a3e92e8f5e4cfe7b1fc9df061ab
Jamille Silvestre <pjdeesilvestre@gmail.com>
2020-01-12 17:34:44 +0800

    edited readme

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
 export const clientId = 'PPyr4kEDrMuT1Q';
 export const clientSecret = 'rtRpHTrHdRn9GJkdX9FCz6a9hgc';
-export const accessToken = '-I9taOEv4M50P4je_ztYTavenKq8';
+export const accessToken = '-4mZhHoM0mS-AVj7EW5oqOTB6Rr8';
